d6a49be20b9848f43777ecf97acf4d8bc59c3e8e
josteph <joshuastephen.roesdiah@my.jcu.edu.au>
2015-09-11 10:45:19 +0800

    FORM PAGE
    -Form page added, included formstyles.css for form page only
    -tablecup.jpg for index.html
    -index.html currently in progress
    -updated css and js

--- js/frias.js
+++ js/frias.js
@@ -1,21 +1,4 @@
 // JavaScript Document
-function validateSearch(){
-	var kwtext = document.formsearch.termtextkeywordsearch.value;
-        document.formsearch.termtextkeywordsearch.value =kwtext.replace(/\'/g,'');
-}
-
-jQuery('#searchsubmit').click(function(){
-	return validateSearch();
-});
-
-if ((!($.browser.mozilla)) && (navigator.appName!="Netscape")) {
-	jQuery('#termtextkeywordsearch').keypress(function(e){
-		if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-			return validateSearch();
-		}
-	});
-}
-
 	function checkFormGN() {
 	var searchForm = document.formsearch;
 	var hasValue=0;
@@ -39,3 +22,73 @@ if ((!($.browser.mozilla)) && (navigator.appName!="Netscape")) {
 			eval('document.'+formName+'.'+field+'.value=""');
 		}
 	}
+	
+/*For slider*/
+var ul;
+var li_items; 
+var li_number;
+var image_number = 0;
+var slider_width = 0;
+var image_width;
+var current = 0;
+function init(){	
+	ul = document.getElementById('image_slider');
+	li_items = ul.children;
+	li_number = li_items.length;
+	for (i = 0; i < li_number; i++){
+			image_width = li_items[i].childNodes[0].clientWidth;
+			slider_width += image_width;
+			image_number++;
+	}
+	
+	ul.style.width = parseInt(slider_width) + 'px';
+	slider(ul);
+}
+
+function slider(){		
+		animate({
+			delay:25,
+			duration: 3000,
+			delta:function(p){return Math.max(0, -1 + 2 * p)},
+			step:function(delta){
+					ul.style.left = '-' + parseInt(current * image_width + delta * image_width) + 'px';
+				},
+			callback:function(){
+				current++;
+				if(current < li_number-1){
+					slider();
+				}
+				else{
+					var left = (li_number - 1) * image_width;					
+					setTimeout(function(){goBack(left)},2000); 				
+					setTimeout(slider, 4000);
+				}
+			}
+		});
+}
+function goBack(left_limits){
+	current = 0;	
+	setInterval(function(){
+		if(left_limits >= 0){
+			ul.style.left = '-' + parseInt(left_limits) + 'px';
+			left_limits -= image_width / 10;
+		}	
+	}, 30);
+}
+function animate(opts){
+	var start = new Date;
+	var id = setInterval(function(){
+		var timePassed = new Date - start;
+		var progress = timePassed / opts.duration
+		if(progress > 1){
+			progress = 1;
+		}
+		var delta = opts.delta(progress);
+		opts.step(delta);
+		if (progress == 1){
+			clearInterval(id);
+			opts.callback();
+		}
+	}, opts.dalay || 17);
+}
+window.onload = init;
